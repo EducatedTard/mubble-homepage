3312551450079d68286dfdc619c4bae831bbe8b2
Daniel Carmichael <daniel.carmichael1@gmail.com>
2016-06-04 23:53:39 -0400

    page presque finie

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var config = require('./config/config');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use(express.static('public'));
+
+mongoose.connect(config.db, config.options);
+var db = mongoose.connection;
+db.on('error', function () {
+  throw new Error('unable to connect to database at ' + config.db);
+});
+
 app.get('/', function(request, response) {
-  response.sendfile(__dirname + '/client/index.html');
+  response.sendFile(__dirname + '/client/index.html');
 });
 
 app.listen(app.get('port'), function() {
